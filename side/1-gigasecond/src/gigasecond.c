be2755f373f095898bcc253c8f059fd8571a5a08
Anandakumar U <anandakumar.u@gmail.com>
2019-11-07 13:05:29 +0530

    solve - side - gigasecond

---
+++ side/1-gigasecond/src/gigasecond.c
@@ -0,0 +1,7 @@
+#include "gigasecond.h"
+
+time_t 
+gigasecond_after (time_t time)
+{
+    return time + 1000000000;
+}
---
+++ side/1-gigasecond/src/gigasecond.h
@@ -0,0 +1,11 @@
+#ifndef __GIGASECOND_H__
+#define __GIGASECOND_H__
+
+#include "stdint.h"
+
+typedef uint64_t time_t;
+
+time_t
+gigasecond_after(time_t time);
+
+#endif /* __GIGASECOND_H__ */
